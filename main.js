3b9b1f806518ac1d80377d6e698565291716acf4
CORP\luizfrib <luiz-fernando.ribeiro@capgemini.com>
2023-06-19 17:40:57 +0100

    Implementing pause.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let down = false;
 let left = false;
 let right = false;
 let shoot = false;
+let pause = false;
 let bulletArray = [];
 let enemyArray = [];
 let ctrlBG1 = -512;
@@ -74,56 +75,63 @@ document.addEventListener("keyup", (event) => {
     case " ":
       shoot = false;
       break;
+
+    case "Enter":
+      pause = !pause;
+    break;
   }
 });
 
 document.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "ArrowUp":
-      up = true;
-      break;
+  if(!pause) {
+    switch (event.key) {
+      case "ArrowUp":
+        up = true;
+        break;
 
-    case "ArrowDown":
-      down = true;
-      break;
+      case "ArrowDown":
+        down = true;
+        break;
 
-    case "ArrowLeft":
-      left = true;
-      break;
+      case "ArrowLeft":
+        left = true;
+        break;
 
-    case "ArrowRight":
-      right = true;
-      break;
+      case "ArrowRight":
+        right = true;
+        break;
 
-    case " ":
-      shoot = true;
-      break;
-  }
+      case " ":
+        shoot = true;
+        break;
+    }
 
-  if (shoot) {
-    bulletArray.push([xAxys + 20, yAxys]);
-  }
+    if (shoot) {
+      bulletArray.push([xAxys + 20, yAxys]);
+    }
 
-  if (up && left) {
-    xCount = -1;
-    yCount = -2;
-  } else if (up && right) {
-    xCount = 1;
-    yCount = -2;
-  } else if (down && left) {
-    xCount = -1;
-    yCount = 2;
-  } else if (down && right) {
-    xCount = 1;
-    yCount = 2;
-  } else if (up) {
-    yCount = -2;
-  } else if (down) {
-    yCount = 2;
-  } else if (left) {
-    xCount = -2;
-  } else if (right) {
-    xCount = 2;
+
+    if (up && left) {
+      xCount = -1;
+      yCount = -2;
+    } else if (up && right) {
+      xCount = 1;
+      yCount = -2;
+    } else if (down && left) {
+      xCount = -1;
+      yCount = 2;
+    } else if (down && right) {
+      xCount = 1;
+      yCount = 2;
+    } else if (up) {
+      yCount = -2;
+    } else if (down) {
+      yCount = 2;
+    } else if (left) {
+      xCount = -2;
+    } else if (right) {
+      xCount = 2;
+    }
   }
 });
 
@@ -141,20 +149,22 @@ function ctrlDrawBg(ctx, bg) {
   ctx.drawImage(bg, 0, ctrlBG2);
   ctx.drawImage(bg, 0, ctrlBG3);
 
-  ctrlBG1 += 2;
-  ctrlBG2 += 2;
-  ctrlBG3 += 2;
+  if(!pause) {
+    ctrlBG1 += 2;
+    ctrlBG2 += 2;
+    ctrlBG3 += 2;
 
-  if (ctrlBG1 > 512) {
-    ctrlBG1 = -512;
-  }
+    if (ctrlBG1 > 512) {
+      ctrlBG1 = -512;
+    }
 
-  if (ctrlBG2 > 512) {
-    ctrlBG2 = -512;
-  }
+    if (ctrlBG2 > 512) {
+      ctrlBG2 = -512;
+    }
 
-  if (ctrlBG3 > 512) {
-    ctrlBG3 = -512;
+    if (ctrlBG3 > 512) {
+      ctrlBG3 = -512;
+    }
   }
 }
 
@@ -176,8 +186,10 @@ function ctrlDrawPlayer(ctx, player1) {
     yAxys = 512;
   }
 
-  xAxys += xCount;
-  yAxys += yCount;
+  if(!pause) {
+    xAxys += xCount;
+    yAxys += yCount;
+  }
 
   ctx.drawImage(player1, xAxys, yAxys);
 }
@@ -185,7 +197,9 @@ function ctrlDrawPlayer(ctx, player1) {
 function ctrlDrawBulletPlayer(ctx, bullet) {
   /* Controla e mostra bullets player */
   bulletArray.forEach((element, index, object) => {
-    element[1] = element[1] - 2;
+    if(!pause) {
+      element[1] = element[1] - 2;
+    }
 
     ctx.drawImage(bullet, element[0], element[1]);
 
@@ -205,7 +219,9 @@ function ctrlDrawEnemy(ctx, enemy) {
   }
 
   enemyArray.forEach((element, index, object) => {
-    element[1] = element[1] + 1;
+    if(!pause) {
+      element[1] = element[1] + 1;
+    }
 
     ctx.drawImage(enemy, element[0], element[1]);
 
@@ -285,4 +301,8 @@ function drawScene(canvas, ctx, bg, player1, bullet, enemy) {
   ctx.font = "19px Arial";
   ctx.fillText("Life: " + life, 10, 30);
   ctx.fillText("Score: " + score, 10, 60);
+
+  if(pause) {
+    ctx.fillText("Paused", 230, 256);
+  }
 }
